d30091844c55988313bf4aefb4a3726aba5b63e7
surya49856 <tejap4985@gmail.com>
2018-03-06 10:08:49 -0700

    Gradlization

---
+++ file.sh
@@ -0,0 +1,4 @@
+cf set-env moviefun S3_ENDPOINTURL http://s3.amazonaws.com
+cf set-env moviefun S3_ACCESSKEY AKIAJH4XODF775RYHNYA
+cf set-env moviefun S3_BUCKETNAME cf-b1e567ce-255f-493a-8255-e56b81c04f98
+cf set-env moviefun S3_SECRETKEY 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
