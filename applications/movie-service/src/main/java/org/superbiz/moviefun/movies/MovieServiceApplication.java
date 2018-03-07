1ff9b8a8e8af4fc3d64036544d8442ba1da2551c
surya49856 <tejap4985@gmail.com>
2018-03-07 09:17:49 -0700

    Completed Movies Microservice

---
+++ applications/movie-service/src/main/java/org/superbiz/moviefun/movies/MovieServiceApplication.java
@@ -0,0 +1,23 @@
+package org.superbiz.moviefun.movies;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MovieServiceApplication {
+
+
+
+
+
+        public static void main(String... args) {
+            SpringApplication.run(MovieServiceApplication.class, args);
+        }
+
+
+
+
+}
